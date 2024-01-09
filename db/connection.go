60cc4ca97f1290cef3dc09aff5088e894d73947e
Ziggy <zigzter@users.noreply.github.com>
2024-01-09 12:32:14 -0800

    Changed database connection to singleton

--- db/connection.go
+++ db/connection.go
@@ -3,12 +3,21 @@ package db
 import (
 	"database/sql"
 	"log"
+	"sync"
+)
+
+var (
+	dbInstance *sql.DB
+	once       sync.Once
+	err        error
 )
 
 func OpenDB() *sql.DB {
-	db, err := sql.Open("sqlite3", "file:chatterm.db?cache=shared&mode=rwc")
-	if err != nil {
-		log.Fatal(err)
-	}
-	return db
+	once.Do(func() {
+		dbInstance, err = sql.Open("sqlite3", "file:chatterm.db?cache=shared&mode=rwc")
+		if err != nil {
+			log.Fatal(err)
+		}
+	})
+	return dbInstance
 }
