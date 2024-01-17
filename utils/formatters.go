602f396e67d7e14be893f2eae669071d2e6e72ef
Ziggy <zigs_17@hotmail.com>
2024-01-17 14:08:17 -0800

    Added a highlighter to special messages, minor icon styling changes, deleted parsecolor file.

--- utils/formatters.go
+++ utils/formatters.go
@@ -15,26 +15,43 @@ func firstMessageColorizer() lipgloss.Style {
 	return lipgloss.NewStyle().Background(lipgloss.Color("#fe640b"))
 }
 
+func highlighter(msg string, color string) string {
+	return lipgloss.NewStyle().
+		BorderStyle(lipgloss.NormalBorder()).
+		BorderForeground(lipgloss.Color(color)).
+		BorderTop(true).
+		BorderRight(true).
+		BorderBottom(true).
+		Padding(0).
+		Margin(0).
+		Render(msg) + "\n"
+}
+
+func iconColorizer(color string) lipgloss.Style {
+	return lipgloss.NewStyle().Foreground(lipgloss.Color(color))
+}
+
 func FormatChatMessage(message types.ChatMessage) string {
 	icon := ""
-	msg := message.Message
-	if message.IsFirstMessage {
-		msg = firstMessageColorizer().Render(msg)
-	}
-	modIcon := "\033[32m󰓥"
-	vipIcon := "\033[35m󰮊"
+	modIcon := iconColorizer("#40a02b").Render("[󰓥]")
+	vipIcon := iconColorizer("#ea76cb").Render("[󰮊]")
 	if message.IsMod {
 		icon = modIcon
 	} else if message.IsVIP {
 		icon = vipIcon
 	}
-	return fmt.Sprintf(
-		"[%s]%s%s: %s\n",
+	msg := fmt.Sprintf(
+		"[%s]%s%s: %s",
 		message.Timestamp,
 		icon,
 		usernameColorizer(message.Color).Render(message.DisplayName),
-		msg,
+		message.Message,
 	)
+	if message.IsFirstMessage {
+		return highlighter(msg, "#e64553")
+	} else {
+		return msg + "\n"
+	}
 }
 
 func FormatSubMessage(message types.SubMessage) string {
@@ -44,47 +61,52 @@ func FormatSubMessage(message types.SubMessage) string {
 	} else {
 		fullMessage = "!"
 	}
-	return fmt.Sprintf(
-		"[%s]%s subscribed for %s months%s\n",
+	msg := fmt.Sprintf(
+		"[%s]%s subscribed for %s months%s",
 		message.Timestamp,
 		usernameColorizer(message.Color).Render(message.DisplayName),
 		message.Months,
 		fullMessage,
 	)
+	return highlighter(msg, "12")
 }
 
 func FormatAnnouncementMessage(message types.AnnouncementMessage) string {
-	return fmt.Sprintf(
-		"[%s][Announcement]%s: %s\n",
+	msg := fmt.Sprintf(
+		"[%s][Announcement]%s: %s",
 		message.Timestamp,
 		usernameColorizer(message.Color).Render(message.DisplayName),
 		message.Message,
 	)
+	return highlighter(msg, "228")
 }
 
 func FormatRaidMessage(message types.RaidMessage) string {
-	return fmt.Sprintf(
-		"[%s]%s raided the channel with %s viewers!\n",
+	msg := fmt.Sprintf(
+		"[%s]%s raided the channel with %s viewers!",
 		message.Timestamp,
 		usernameColorizer(message.Color).Render(message.DisplayName),
 		message.ViewerCount,
 	)
+	return highlighter(msg, "12")
 }
 
 func FormatGiftSubMessage(message types.SubGiftMessage) string {
-	return fmt.Sprintf(
-		"[%s]%s gifted a subscription to %s\n",
+	msg := fmt.Sprintf(
+		"[%s]%s gifted a subscription to %s",
 		message.Timestamp,
 		usernameColorizer(message.Color).Render(message.GiverName),
 		message.ReceiverName,
 	)
+	return highlighter(msg, "12")
 }
 
 func FormatMysteryGiftSubMessage(message types.MysterySubGiftMessage) string {
-	return fmt.Sprintf(
-		"[%s]%s is giving %s subs to the channel!\n",
+	msg := fmt.Sprintf(
+		"[%s]%s is giving %s subs to the channel!",
 		message.Timestamp,
 		usernameColorizer(message.Color).Render(message.GiverName),
 		message.GiftAmount,
 	)
+	return highlighter(msg, "9")
 }
