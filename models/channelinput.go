ee859213dc7e43af81c0185adcb29c26f7b7b1cf
Ziggy <zigzter@users.noreply.github.com>
2024-01-26 16:46:32 -0800

    Added initial live channel paging logic

--- models/channelinput.go
+++ models/channelinput.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/charmbracelet/bubbles/key"
+	"github.com/charmbracelet/bubbles/paginator"
 	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
@@ -22,12 +24,15 @@ var (
 )
 
 type ChannelInputModel struct {
-	textinput    textinput.Model
-	authRequired bool
-	liveStreams  []types.LiveChannelsData
-	error        string
-	ac           *utils.Trie
-	width        int
+	textinput         textinput.Model
+	authRequired      bool
+	liveStreams       []types.LiveChannelsData
+	error             string
+	ac                *utils.Trie
+	width             int
+	height            int
+	paginator         paginator.Model
+	streamsViewHeight int
 }
 
 func InitialChannelInputModel() ChannelInputModel {
@@ -44,6 +49,19 @@ func InitialChannelInputModel() ChannelInputModel {
 		ti.SetValue(configChannel)
 	}
 	ti.Focus()
+	p := paginator.New()
+	p.Type = paginator.Dots
+	p.PerPage = 10
+	p.ActiveDot = lipgloss.NewStyle().
+		Foreground(lipgloss.AdaptiveColor{Light: "235", Dark: "252"}).
+		Render("•")
+	p.InactiveDot = lipgloss.NewStyle().
+		Foreground(lipgloss.AdaptiveColor{Light: "250", Dark: "238"}).
+		Render("•")
+		// TODO: these ctrl binds don't work
+	p.KeyMap.NextPage = key.NewBinding(key.WithKeys("right", "ctrl+l"))
+	p.KeyMap.PrevPage = key.NewBinding(key.WithKeys("left", "ctrl+h"))
+	model.paginator = p
 	model.textinput = ti
 	model.authRequired = authRequired
 	fetchLiveStreams(&model)
@@ -69,6 +87,7 @@ func fetchLiveStreams(m *ChannelInputModel) {
 		m.error = err.Error()
 	} else {
 		m.liveStreams = liveStreamsResp.Data
+		m.paginator.SetTotalPages(len(m.liveStreams))
 		var streamNames []string
 		for _, stream := range liveStreamsResp.Data {
 			streamNames = append(streamNames, stream.UserName)
@@ -83,7 +102,6 @@ func (m ChannelInputModel) Init() tea.Cmd {
 }
 
 func (m ChannelInputModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	var cmd tea.Cmd
 	switch msg := msg.(type) {
 	case tea.WindowSizeMsg:
 		m.width = msg.Width
@@ -115,8 +133,10 @@ func (m ChannelInputModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		}
 	}
 	m.authRequired = utils.IsAuthRequired()
-	m.textinput, cmd = m.textinput.Update(msg)
-	return m, cmd
+	var tiCmd, pCmd tea.Cmd
+	m.textinput, tiCmd = m.textinput.Update(msg)
+	m.paginator, pCmd = m.paginator.Update(msg)
+	return m, tea.Batch(tiCmd, pCmd)
 }
 
 func (m ChannelInputModel) View() string {
@@ -128,11 +148,9 @@ func (m ChannelInputModel) View() string {
 		if m.error != "" {
 			b.WriteString("Live channel retrieval error:" + m.error)
 		}
-		// TODO: add pagination
-		for i, channel := range m.liveStreams {
-			if i > 10 {
-				break
-			}
+		start, end := m.paginator.GetSliceBounds(len(m.liveStreams))
+		totalHeight := 0
+		for _, channel := range m.liveStreams[start:end] {
 			liveChannel := fmt.Sprintf(
 				"%s playing %s: %s (%s viewers)\n",
 				channelNameStyle.Render(channel.UserName),
@@ -140,8 +158,13 @@ func (m ChannelInputModel) View() string {
 				titleStyle.Render(channel.Title),
 				viewerCountStyle.Render(fmt.Sprintf("%d", channel.ViewerCount)),
 			)
-			b.WriteString(wordwrap.String(liveChannel, m.width-4))
+			wrapped := wordwrap.String(liveChannel, m.width-4)
+			// Single lines return 2 from Height, so we're subtracting 1
+			totalHeight += lipgloss.Height(wrapped) - 1
+			b.WriteString(wrapped)
 		}
+		m.streamsViewHeight = totalHeight
+		b.WriteString("  " + m.paginator.View())
 		b.WriteString("\nEnter channel name:\n")
 		b.WriteString(m.textinput.View() + "\n")
 	}
